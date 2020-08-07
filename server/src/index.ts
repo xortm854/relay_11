0f78a026ef4a77e78ead41012ae47eaa502f0baf
Changehee Choi <changhee.dev@gmail.com>
2020-08-07 17:36:05 +0900

    feat: typeorm 설정 추가

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,16 @@
 import Koa from "koa";
+import "reflect-metadata";
+import { createConnection } from "typeorm";
 
 const main = async function () {
     const app = new Koa();
     // const router = new Router();
+    const connection = createConnection().then(() => {
+        console.log("connected");
+    });
 
     app.listen(8080, () => {});
 };
 
 main();
+
