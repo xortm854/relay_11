3296d987488ad146cd6cb9ead49026586841609b
lacomaco <lacomaco122@gmail.com>
2020-08-14 18:45:10 +0900

    6:44 연결남음

--- front/src/pages/Photo.js
+++ front/src/pages/Photo.js
@@ -8,7 +8,8 @@ import Nav from 'react-bootstrap/Nav'
 import { Link } from "react-router-dom";
 import axios from 'axios';
 import addPhoto from './addPhoto1.png';
-import {useState,useCallback,useMemo,useEffect} from 'react';
+import './Photo.css';
+import {useState,useCallback,useMemo,useEffect,useRef} from 'react';
 
 const PhotoHeader = styled.header`
 display: flex;
@@ -40,12 +41,12 @@ const apiSecret = 'RuhsLmEX55';
 const val = {fontWeight:'bold'}
 const Photo = ()=>{
     const [imgObject,setImgObject] = useState(null);
-    const [imgSize,setImgSize] = useState('');
-    
-    const sendAPI = useCallback((e)=>{
-        const formData = new FormData();
+    const [manLength,setManLength] = useState(0);
+    const imageDom = useRef(null);
 
-    },[imgObject]);
+    const manBedges = useMemo(()=>{
+
+    },[manLength]);
 
     const testAPI = useEffect(()=>{
         console.log('call');
@@ -53,8 +54,21 @@ const Photo = ()=>{
             requestApi(imgObject);
         }
     },[imgObject]);
+
+    const imageChange = useCallback((e)=>{
+        let inputNode = e.target;
+        let loadedFiles = inputNode.files;
+        let image = imageDom.current;
+        requestApi(loadedFiles[0]);
+        image.src = window.URL.createObjectURL(loadedFiles[0]);
+        image.onload = (e) => {
+            window.URL.revokeObjectURL(e.target.src);
+        };
+        setImgObject(loadedFiles[0]);
+    },[]);
     
     return (
+    
         <div style={{backgroundColor:'#F7F9FA'}}>
             <div>
                 <PhotoHeader>
@@ -75,27 +89,38 @@ const Photo = ()=>{
             <PhotoMain>
                 <ImageWrapper>
                     {/* <Image id="image" src="https://user-images.githubusercontent.com/50394490/90222363-e2a89f00-de46-11ea-8ae7-ba3007602d84.png" fluid /> */}
-                    <Image id="image" src={addPhoto} fluid />
+                    <Image id="image" src={addPhoto} fluid ref={imageDom} />
                 </ImageWrapper>
+                <div className="image-tag" style={{position:'relative',width:'80px',height:'30px'}}>
+                    <input style={{width:'80px',height:'30px', position:'absolute'}}
+                        onSubmit={(e)=>{
+                            e.target.nextElementSibling.style.display='block';
+                        }}
+                        /*{ 질문가능해요 -> 통신할때 axios 안쓰시고 fetch 쓰시는건가요?? 지금 프론트에서는 fetch 썼어요. 편하신걸로 골라 쓰시면 될것 같아요. }
+                        
+                        */
+                    ></input>
+                    <Badge className="tag" style={{width:'80px',height:'30px' , position:'absolute'}} variant="secondary"
+                        onClick={(e)=>{
+                            e.target.style.display='none';
+                        }}
+                    >New</Badge> 
+                </div>
+                
+                {/* <Badge variant="secondary">New</Badge>
+                <Badge variant="secondary">New</Badge>
+                <Badge variant="secondary">New</Badge> */}
                 <PhotoFooter>
                     <input style={{width:'60%'}} value="사진을 첨부해주세요." disabled></input>
-                    <Button variant="secondary" size="sm">사진 첨부 버튼</Button>
+                    <Button variant="secondary" size="sm">
+                        <label htmlFor="imageInput" style={{marginBottom:'0px'}}>
+                            사진 첨부 버튼
+                        </label>
+                    </Button>
                     {/* <Button variant="primary"  size="sm">작성 하기</Button> */}
-                    <input style={{backgroundColor:'green'}}
+                    <input id="imageInput" style={{backgroundColor:'green',visibility:'hidden',width:'1px'}}
                         type="file" 
-                        onChange={(e)=>{
-                            let inputNode = e.target;
-                            let loadedFiles = inputNode.files;
-                            let image = document.querySelector('#image');
-                            requestApi(loadedFiles[0]);
-                            // image.src = window.URL.createObjectURL(loadedFiles[0]);
-                            // image.onload = (e) => {
-                            //     window.URL.revokeObjectURL(e.target.src);
-                            // };
-                            setImgObject(loadedFiles[0]);
-                            setImgSize(loadedFiles[0].size);
-                            // requestApi(loadedFiles[0]);
-                        }}>
+                        onChange={imageChange}>
                     </input>
                 </PhotoFooter>
             </PhotoMain>
