380cf3313c8bed343d94f7d630239a2f2584488f
Eunchae Jeon <eunchaejeon@Eunchaeui-MacBookPro.local>
2020-08-15 14:28:40 +0900

    add week3 backend

---
+++ week3-back/app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path')
+const request = require('request');
+const cors = require('cors');
+const corsOptions = {
+    origin: 'http://localhost:3000', // 허락하고자 하는 요청 주소
+    credentials: true, // true로 하면 설정한 내용을 response 헤더에 추가 해줍니다.
+};
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+
+const client_id = '2rPyGSTC49Dfplyx5UvD';
+const client_secret = 'RuhsLmEX55';
+const api_url = 'https://openapi.naver.com/v1/vision/face'; // 얼굴 감지
+
+const fs = require('fs');
+const multer = require('multer');  //multer 모듈 import
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, 'image/');
+        },
+        filename: function (req, file, cb) {
+            cb(null, new Date().valueOf() + path.extname(file.originalname));
+        }
+    }),
+});
+
+app.use(express.static(path.join(__dirname, '/build')));
+
+app.get('/', (req, res) => {
+  res.send(express.static(path.join(__dirname, '/build/index.html')));
+})
+
+
+// key값 img
+app.post('/face', upload.single('img'), function (req, res) {
+  console.log("face click");
+    var imgFile = req.file; //파일이 1개인 경우(upload.single()을 이용한 경우)
+
+    var _formData = {
+        image: 'image',
+        image: fs.createReadStream(__dirname + '/'+imgFile.path)
+    };
+    var _req = request.post({
+        url: api_url, formData: _formData,
+        headers: { 'X-Naver-Client-Id': client_id, 'X-Naver-Client-Secret': client_secret }
+    }).on('response', function (response) {
+    });
+    _req.pipe(res); // 브라우저로 출력
+});
+
+
+app.listen(3000, () => console.log('Server is running on port 3000...'));
---
+++ week3-back/build/precache-manifest.5689e2bd2b44fa21a465a5a1c5022dc6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa5d13d3c7475a9bdf96559bf5b1fb28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74e797b772fd72c05415",
+    "url": "/static/css/2.06c478a6.chunk.css"
+  },
+  {
+    "revision": "d08ba5e5e5e27c72b368",
+    "url": "/static/css/main.d4cf13a0.chunk.css"
+  },
+  {
+    "revision": "74e797b772fd72c05415",
+    "url": "/static/js/2.f4c2901e.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.f4c2901e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d08ba5e5e5e27c72b368",
+    "url": "/static/js/main.94d41ce3.chunk.js"
+  },
+  {
+    "revision": "e80322743b908939bbdf",
+    "url": "/static/js/runtime-main.a80a517c.js"
+  }
+]);
