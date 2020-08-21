0c900eef37e4460f24899de3ee8984f3e74c6e4c
saeeng <saeeng.op@gmail.com>
2020-08-21 15:24:14 +0900

    week4-back

---
+++ week4-back/routes/test/service/getTest.js
@@ -0,0 +1,6 @@
+module.exports = function (req, res) {
+
+    res.json("안녕하세요")
+
+
+}
---
+++ week4-back/sequelize/config.js
@@ -0,0 +1,23 @@
+
+require('path');
+require('dotenv')
+
+
+// module.exports = {
+//   port: process.env.SQL_PORT,
+//   username: process.env.SQL_USERNAME,
+//   password: process.env.SQL_PASSWORD,
+//   database: process.env.SQL_DATABASE,
+//   host: process.env.SQL_HOST,
+//   dialect: "mysql",
+// }
+
+//;;;;
+module.exports = {
+  port: "3306",
+  username: "relay16",
+  password: "Boostcamp!",
+  database: "relay16",
+  host: "db-4lhue.pub-cdb.ntruss.com",
+  dialect: "mysql",
+}
